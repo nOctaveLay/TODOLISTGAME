8413ab06bc9c02ca081122e210ba370b66e156b4
Clear <wjddkdml1124@naver.com>
2017-06-01 08:02:39 +0900

    test.py를 실행시키면 이제 일이 끝날 때마다 아이템이 튀어나온다.

--- test.py
+++ test.py
@@ -1,21 +1,29 @@
 from todolist import *
 from random_item import Item
 if __name__ == '__main__':
-	a = List_Do()
+	a = Do()
 	pick = Item()
-	pick.theme_pick()
 	while True:
+		print()
+		a.list()
+		print()
 		print("0 : end")
 		print("1 : add the work")
 		print("2 : clear the work")
+		print()
 		input_number = input("choose the number: ")
-		if input_number.isdigit() == False or int(input_number) > 3 or int(input_number) < 0:
+		if input_number.isdigit() == False or int(input_number) < 0 or int(input_number) > 3:
 			print("input again")
+
 		elif int(input_number) == 1:
 			a.add()
-			print(a.r_list)
+			a.update()
+			print("successfully add")
+		
 		elif int(input_number) == 2:
 			a.clear()
-		else:
 			a.update()
+			pick.item_pick()
+			
+		else: #int(input_number) == 0
 			break
