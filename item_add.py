689fc8be86739f4e5b3e19b6a89b3ec6647307f0
Clear <wjddkdml1124@naver.com>
2017-06-02 20:06:26 +0900

    아이템 폴더 안에 있는 아이템을 더하도록 했습니다!

--- item_add.py
+++ item_add.py
@@ -1,8 +1,8 @@
 from random_item import *
 item = Item()
-item.show_item_list()
 while True:
-	a = input("0")
+	item.show_item_list(item.list)
+	a = input("0 ")
 	if a == "0":
 		item.update()
 		break
